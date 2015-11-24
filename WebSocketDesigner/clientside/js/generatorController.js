33c35964af2a8482243c78b35cd9834a4f0df5a8
Eric <jans-eric@live.nl>
2015-11-24 13:49:14 +0100

    Added code generating

--- WebSocketDesigner/clientside/js/generatorController.js
+++ WebSocketDesigner/clientside/js/generatorController.js
@@ -3,9 +3,18 @@ theApp.controller('generatorController', ['$scope', '$location', function ($scop
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/yaml");
+    editor.$blockScrolling = Infinity;
 
     var generated = ace.edit("generated");
     generated.setTheme("ace/theme/monokai");
     generated.getSession().setMode("ace/mode/javascript");
+    generated.$blockScrolling = Infinity;
 
+    $scope.Generate = function(){
+      var input = editor.getSession().getValue();
+      var output = jsyaml.safeLoad(input);
+      output = JSON.stringify(output, null, 4);
+      generated.setValue(output, 1);
+    }
 }]);
+
