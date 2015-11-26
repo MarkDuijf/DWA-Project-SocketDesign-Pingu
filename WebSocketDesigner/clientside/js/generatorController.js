72b1818f55e6907e05fcaaf21bdd1234225df5b6
Eric <jans-eric@live.nl>
2015-11-26 11:58:46 +0100

    more code generation

--- WebSocketDesigner/clientside/js/generatorController.js
+++ WebSocketDesigner/clientside/js/generatorController.js
@@ -1,38 +1,51 @@
 theApp.controller('generatorController', ['$scope', '$location', function ($scope) {
 
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/yaml");
-    editor.$blockScrolling = Infinity;
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/yaml");
+  editor.$blockScrolling = Infinity;
 
-    var generated = ace.edit("generated");
-    generated.setTheme("ace/theme/monokai");
-    generated.getSession().setMode("ace/mode/javascript");
-    generated.$blockScrolling = Infinity;
+  var generated = ace.edit("generated");
+  generated.setTheme("ace/theme/monokai");
+  generated.getSession().setMode("ace/mode/javascript");
+  generated.$blockScrolling = Infinity;
 
-
-    $scope.error = null;
-    $scope.Generate = function(){
-      try {
-        //var actionArray = [];
-        var input = editor.getSession().getValue();
-        var output = jsyaml.safeLoad(input);
-        var pathArray = Object.keys(output.paths);
-        for(var i = 0; i < Object.keys(output.paths).length; i++)
-        {
-          console.log(pathArray);
-          console.log(output.paths[pathArray[i]]);
-          for(var x = 0; x < Object.keys(output.paths[pathArray[i]]).length; x++) {
+  $scope.error = null;
+  $scope.Generate = function () {
+    try {
+      var output = [];
+      var input = editor.getSession().getValue();
+      input = jsyaml.safeLoad(input);
+      if (input.paths != undefined) {
+        var pathArray = Object.keys(input.paths);
+        for (var i = 0; i < Object.keys(input.paths).length; i++) {
+          var actionArray = [input.paths[pathArray[i]].POST, input.paths[pathArray[i]].GET, input.paths[pathArray[i]].PUT];
+          for (var x = 0; x < Object.keys(input.paths[pathArray[i]]).length; x++) {
+            if (actionArray[x] != undefined) {
+              switch (x) {
+                case 0:
+                  console.log(actionArray[x]);
+                  break;
+                case 1:
+                  console.log(actionArray[x]);
+                  break;
+                case 2:
+                  console.log(actionArray[x]);
+                  break;
+              }
+            }
           }
         }
-        output = JSON.stringify(output, null, 4);
-        generated.setValue(output, 1);
+        input = JSON.stringify(input, null, 4);
+        generated.setValue(input, 1);
         $scope.error = null;
       }
-      catch(e){
-        scroll(0,0);
-        generated.setValue('',1);
-        $scope.error = e.message;
-      }
     }
+    catch (e) {
+      console.log(e);
+      scroll(0, 0);
+      generated.setValue('', 1);
+      $scope.error = e.message;
+    }
+  }
 }]);
