0181e042de73d6a4fabbdb72ae39369b892e8692
sebastiaan <lha.vonk@student.han.nl>
2016-01-04 15:52:06 +0100

    meer changePassword tests

--- WebSocketDesigner/Tests/changePasswordTest.js
+++ WebSocketDesigner/Tests/changePasswordTest.js
@@ -13,7 +13,8 @@ describe('Als een gebruiker zijn/haar wachtwoord wil veranderen', function(){
         var change = {
             username: 'test',
             password: 'test',
-            newPass: 'dit is een veel te lang wachtwoord'
+            newPass: 'dit is een veel te lang wachtwoord',
+            newPassR: 'dit is een veel te lang wachtwoord'
         };
 
         agent
@@ -25,6 +26,116 @@ describe('Als een gebruiker zijn/haar wachtwoord wil veranderen', function(){
             .end(function(err,res) {
                 expect(err).to.be.null;
                 expect(res.text).to.equal("Data error");
+                done();
+            });
+    });
+
+    it('een te kort wachtwoord geweigerd worden', function(done){
+
+        var change = {
+            username: 'test',
+            password: 'test',
+            newPass: 'jo',
+            newPassR: 'jo'
+        };
+
+        agent
+            .post('/confirmPasswordChange')
+            .send(change)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res){
+                expect(err).to.be.null;
+                expect(res.text).to.equal("Data error");
+                done();
+            });
+    });
+
+    it('een wachtwoord geweigerd worden als de herhaling niet hetzelfde is als het eerste vernieuwde wachtwoord', function(done){
+
+        var change = {
+            username: 'test',
+            password: 'test',
+            newPass: 'wachtwoord',
+            newPassR: 'wachtwoord1'
+        };
+
+        agent
+            .post('/confirmPasswordChange')
+            .send(change)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res){
+                expect(err).to.be.null;
+                expect(res.text).to.equal("Data error");
+                done();
+            });
+    });
+
+    it('een wachtwoord geweigerd worden als de ingevoerde waarde \'\' is', function(done){
+
+        var change = {
+            username: 'test',
+            password: 'test',
+            newPass: '',
+            newPassR: 'wachtwoord'
+        };
+
+        agent
+            .post('/confirmPasswordChange')
+            .send(change)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res){
+                expect(err).to.be.null;
+                expect(res.text).to.equal("Data error");
+                done();
+            });
+    });
+
+    it('een wachtwoord geweigerd worden als de herhaling de waarde \'\' heeft', function(done){
+
+        var change = {
+            username: 'test',
+            password: 'test',
+            newPass: 'wachtwoord',
+            newPassR: ''
+        };
+
+        agent
+            .post('/confirmPasswordChange')
+            .send(change)
+            .set('Content-Type', 'application/json')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res){
+                expect(err).to.be.null;
+                expect(res.text).to.equal("Data error");
+                done();
+            });
+    });
+
+    it('moet een geldig wachtwoord geaccepteerd worden', function(done){
+
+        var change = {
+            username: 'test',
+            password: 'test',
+            newPass: 'wachtwoord',
+            newPassR: 'wachtwoord'
+        };
+
+        agent
+            .post('/confirmPasswordChange')
+            .send(change)
+            .set('Content-Type', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res){
+                expect(err).to.be.null;
+                expect(res.text).to.equal(change.newPass);
                 done();
             });
     });
