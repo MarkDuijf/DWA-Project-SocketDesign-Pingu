fdea2f8061811af8b65443ad0387f9fea2381ae8
Vincent van Rossum <vincent_vanrossum@hotmail.com>
2016-01-06 10:41:52 +0100

    changed to homepage

--- WebSocketDesigner/clientside/assets/js/controllers/main.js
+++ WebSocketDesigner/clientside/assets/js/controllers/main.js
@@ -52,6 +52,9 @@ theApp.factory('usernameFactory', function ($http) {
     object.setUsername = function(userName) {
         object.userName = userName;
     };
+    object.setfirstName = function(name) {
+        object.name = name;
+    };
     return object;
 });
 
@@ -74,6 +77,7 @@ theApp.controller('homeController', function ($scope, $http, $routeParams, $time
 
         $scope.loggedIn = LoginFactory.loggedIn;
         $scope.loggedInUser = usernameFactory.userName;
+        $scope.loggedInUserfirstName = usernameFactory.firstName;
         $scope.homeMessage = "No message";
         $scope.showHomeMessage = false;
         $scope.isErrorMessage = false;
@@ -108,7 +112,9 @@ theApp.controller('homeController', function ($scope, $http, $routeParams, $time
                 if (data.loggedIn === "Logged in") {
                     LoginFactory.setLogin(true);
                     usernameFactory.setUsername(data.username);
+                    usernameFactory.setfirstName(data.firstName);
                     $scope.loggedInUser = data.username;
+                    $scope.loggedInUserfirstName = data.firstName;
                     $scope.loggedIn = true;
                 } else if (data.loggedIn === "Not logged in") {
                     LoginFactory.setLogin(false);
@@ -191,6 +197,8 @@ theApp.controller('homeController', function ($scope, $http, $routeParams, $time
                 $scope.homeMessage = "You have been logged in (this is a placeholder)";
                 $scope.isErrorMessage = false;
                 usernameFactory.setUsername($scope.loginData.username);  // $scope.loginData.username;
+                usernameFactory.setfirstName($scope.loginData.firstName);
+                $scope.loggedInUserfirstName = data;
                 $scope.loggedInUser = $scope.loginData.username;
                 LoginFactory.setLogin(true);
             }).
